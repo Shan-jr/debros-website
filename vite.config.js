2c0a5e7cd301326fe440dedc6381045319e8c203
Shan-jr <135948573+Shan-jr@users.noreply.github.com>
2025-08-02 16:20:28 +0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Website-Debros-New/', // important for GitHub Pages
+  base: "/", // IMPORTANT: Use "/" for Vercel deployment
 })
